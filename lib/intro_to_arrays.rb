ec87312cd28d7b5ec9ba8874bfd8b509a2191497
Derek Cerretani <derekcerretani@gmail.com>
2018-12-19 22:45:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,3 +10,13 @@ def first_element(array)
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   taylor_swift[0]
 end
+
+def third_element(array)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift[2]
+end
+
+def last_element(array)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift[-1]
+end
