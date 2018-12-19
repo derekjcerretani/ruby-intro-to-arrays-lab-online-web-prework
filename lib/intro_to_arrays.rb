36025b679e020e637b7b282e8853009a027d9984
Derek Cerretani <derekcerretani@gmail.com>
2018-12-19 22:50:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -24,5 +24,6 @@ end
 south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
 
 def first_element_with_array_methods(array)
-  south_east_asia.first 
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  south_east_asia.first
 end
