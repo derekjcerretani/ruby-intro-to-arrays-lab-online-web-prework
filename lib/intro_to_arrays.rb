8c8344997afdb8dcfe46a666ab5f96152e74bd95
Derek Cerretani <derekcerretani@gmail.com>
2018-12-19 22:59:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,8 +6,6 @@ def array_with_two_elements
   my_two_array = ["cats", "dogs"]
 end
 
-taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-
 def first_element(array)
   array[0]
 end
@@ -20,8 +18,6 @@ def last_element(array)
   array[-1]
 end
 
-south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-
 def first_element_with_array_methods(array)
   array.first
 end
@@ -29,8 +25,6 @@ end
 def last_element_with_array_methods(array)
   array.last
 end
-
-programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
 
 def length_of_array(array)
   array.length
